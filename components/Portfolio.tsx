fba8896c883c496ffbbe9aae629065310e1b1f7b
giuliocapecchi <giuliocapecchi2000@gmail.com>
2024-11-27 23:03:36 +0100

    Adjust layout and text sizes for mobile responsiveness in Home and Portfolio components

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Project } from "@/types/interfaces";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+import { isMobile } from 'react-device-detect';
 
 interface PortfolioProps {
     projects: Project[];
@@ -16,13 +17,16 @@ const Portfolio: React.FC<PortfolioProps> = ({ projects }) => {
         return () => clearTimeout(timeout);
     }, []);
 
+    const textSize = isMobile ? "text-sm" : "text-lg";
+    const headerSize = isMobile ? "text-lg" : "text-2xl";
+
     return (
         <div
             className={`portfolio transition-opacity duration-500 ${
                 fadeIn ? "opacity-100" : "opacity-0"
             } text-white text-center p-3 bg-gray-600/30 bg-opacity-75 rounded-lg shadow-lg max-w-4xl mx-auto`}
         >
-            <h1 className="text-2xl mb-4">Past Projects</h1>
+            <h1 className={`${headerSize} mb-4`}>Past Projects</h1>
             <ul className="list-none p-0">
                 {projects.map((project) => (
                     <li key={project.id} className="mb-6 flex flex-col items-center">
@@ -30,13 +34,13 @@ const Portfolio: React.FC<PortfolioProps> = ({ projects }) => {
                             href={project.html_url}
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="text-lg font-bold hover:text-sky-400/80 transition-colors duration-300 flex items-center justify-center"
+                            className={`font-bold hover:text-sky-400/80 transition-colors duration-300 flex items-center justify-center ${textSize}`}
                         >
                             <FontAwesomeIcon icon={faGithubSquare} size="lg" className="mr-2" />
                             {project.name}
                         </a>
                         {project.description && (
-                            <p className="text-sm mt-2 text-gray-300 text-center">
+                            <p className={`mt-2 text-gray-300 text-center ${textSize}`}>
                                 {project.description}
                             </p>
                         )}
