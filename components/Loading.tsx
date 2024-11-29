719b404affc2362eb55fd06bb2fd58bcca51809a
giuliocapecchi <giuliocapecchi2000@gmail.com>
2024-11-29 16:23:23 +0100

    Update loader styles and remove loading text

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -10,7 +10,6 @@ const Loading: React.FC<LoadingProps> = ({ fadeOut }) => {
     return (
         <div className={`loading ${fadeOut ? 'fade-out' : ''}`} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', color: '#fff', backgroundColor: 'black' }}>
             <div className="loader"></div>
-            <p className="ml-4 text-zinc-400">Loading...</p>
         </div>
     );
 };
